91af75b5cb090d2a40177e37c7a3f501c92022f7
wtfdivyansh <divyanshgupta19305@gmail.com>
2026-04-02 21:53:30 +0530

    add 82 comps

---
+++ src/data/contents/components/alerts/config.ts
@@ -0,0 +1,6 @@
+export const category = {
+  slug: 'alerts',
+  label: 'Alerts',
+  description:
+    'A component used to display important messages, notifications, or feedback to the user.',
+};
---
+++ src/data/contents/components/alerts/variant-8.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { useState } from 'react';
+
+import { MdError } from 'react-icons/md';
+import { RxCross2 } from 'react-icons/rx';
+
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from '@/components/base-ui/alert';
+import { Button } from '@/components/base-ui/button';
+
+const Alert8 = () => {
+  const [isActive, setIsActive] = useState(true);
+
+  if (!isActive) return null;
+
+  return (
+    <Alert className=" bg-primary text-primary-foreground flex justify-between border-none">
+      <MdError className="size-4" />
+      <div className="flex flex-1 flex-col gap-4">
+        <div className="flex-1 flex-col justify-center gap-1">
+          <AlertTitle>Update ready to install</AlertTitle>
+          <AlertDescription className="text-primary-foreground/70 text-wrap!">
+            A new version is available with performance improvements and a
+            refreshed dashboard experience.
+          </AlertDescription>
+        </div>
+        <div className="flex items-center gap-4">
+          <Button className="bg-secondary/10 hover:bg-secondary/20 focus-visible:bg-secondary/20 h-7 cursor-pointer rounded-lg px-2">
+            Remind me later
+          </Button>
+          <Button
+            variant="secondary"
+            className="h-7 cursor-pointer rounded-lg px-2"
+          >
+            Update now
+          </Button>
+        </div>
+      </div>
+      <button
+        className="size-5 cursor-pointer"
+        onClick={() => setIsActive(false)}
+      >
+        <RxCross2 className="size-5" />
+        <span className="sr-only">Close</span>
+      </button>
+    </Alert>
+  );
+};
+
+export default Alert8;
